c5661869d0e923639a4a43e8497599d6328668b2
MKrzek <mkrzek@googlemail.com>
2020-05-04 15:48:00 +0100

    feat: add autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
+//autobind decorator
+function autobind(_: any, _2:string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    console.log(originalMethod)
+    console.log(descriptor)
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+       get(){
+           const boundFn = originalMethod.bind(this);
+           return boundFn
+       }
+    }
+    return adjDescriptor
+}
+
+//ProjectInput Class
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -26,13 +42,13 @@ class ProjectInput {
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
     };
-
+    @autobind
     private submitHandler(event: Event){
         event.preventDefault();
-        console.log('suuu', this.titleInputElement)
+        console.log('suuu', this.titleInputElement.value)
     }
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler)
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
 }
