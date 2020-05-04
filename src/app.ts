f281cc842310837a18b0ab407517c67da757f763
MKrzek <mkrzek@googlemail.com>
2020-05-04 15:34:36 +0100

    feat: add submit eventListener

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
-    element: HTMLFormElement
+    element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement
+
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,11 +13,26 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.attach()
+        this.element.id = 'user-input'
 
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure()
+        this.attach()
     }
-    private attach(){
+
+    private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    };
+
+    private submitHandler(event: Event){
+        event.preventDefault();
+        console.log('suuu', this.titleInputElement)
+    }
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
 }
